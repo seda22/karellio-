e8a810332c3d50a48f94c41b597c4020e26791ca
seda22 <sedakacarr@gmail.com>
2026-02-24 05:20:48 +0300

    F-02 Statik destinasyon veri modeli oluşturuldu
    - 6 destinasyon eklendi
    - Ulaşım, konaklama, yeme-içme ve aktivite alanları tanımlandı
    - Proje iş analizine uygun JSON yapısı kuruldu

--- Frontend/src/data/Destinations.js
+++ Frontend/src/data/Destinations.js
@@ -1,30 +1,167 @@
 const destinations = [
     {
-        id: "",
-        name: "",
-        country: "",
-        description: "",
+        id: "chios-001",
+        name: "Sakız Adası (Chios)",
+        country: "Yunanistan",
+        description: "Orta Çağ köyleri ve damla sakızı bahçeleriyle ünlü, İzmir'e en yakın kültürel durak.",
         transportation: [{
-            type: "",
-            description: ""
+            type: "Feribot",
+            description: "Çeşme'den 20-30 dakikada ulaşım."
         }],
         accommodation: [{
-            name: "",
-            type: "",
-            description: ""
+            name: "Mestass Mastic",
+            type: "Butik Otel",
+            description: "Tarihi taş ev konaklaması."
 
         }],
         foodAndDrink: [{
-            name: "",
-            type: "",
-            description: ""
+            name: "Mestass Taverna",
+            type: "Restoran",
+            description: "Geleneksel deniz mahsülleri."
 
         }],
         activities: [{
-            name: "",
-            description: ""
+            name: "Köy Gezisi",
+            description: "Geometrik desenli Pyrgi evlerini keşfetme."
 
         }],
-    }]
+    },
+    {
+        id: "rhodes-002",
+        name: "Rodos",
+        country: "Yunanistan",
+        description: "Şövalyeler adası; UNESCO mirası olan devasa bir kale içi şehri ve Lindos'un beyaz evleri.",
+        transportation: [{
+            type: "Katamaran",
+            description: "Marmaris'ten 1 saat."
+        }],
+        accommodation: [{
+            name: "Speranzza",
+            type: "Tarihi Konak",
+            description: "Şövalyeler Caddesi'ne yakın butik otel."
+
+        }],
+        foodAndDrink: [{
+            name: "Marco Poloo",
+            type: "Fine Dining",
+            description: "Eski şehir içinde gurme akşam yemeği."
+
+        }],
+        activities: [{
+            name: "Lindos Turu",
+            description: "Akropol ziyareti ve eşsiz manzara."
+
+        }],
+    },
+    {
+        id: "kos-003",
+        name: "Kos (İstanköy)",
+        country: "Yunanistan",
+        description: "Tıbbın babası Hipokrat'ın memleketi; geniş bisiklet yolları ve plajlarıyla popüler.",
+        transportation: [{
+            type: "Hızlı Feribot",
+            description: "Bodrum'dan 20-45 dakika."
+        }],
+        accommodation: [{
+            name: "Albergo Gelsominoss",
+            type: "Otel",
+            description: "Tarihi mimaride lüks konaklama."
+
+        }],
+        foodAndDrink: [{
+            name: "Orommedon",
+            type: "Restoran",
+            description: "Zia köyünde gün batımı eşliğinde yemek."
+
+        }],
+        activities: [{
+            name: "Bisiklet Turu",
+            description: "Ada merkezinde bisikletle sahil gezisi."
+
+        }],
+    },
+    {
+        id: "lesvos-004",
+        name: "Midilli (Lesvos)",
+        country: "Yunanistan",
+        description: "Uzo'nun başkenti; devasa bir ada, taş sokaklı Molivos kasabası ve gurme durakları.",
+        transportation: [{
+            type: "Feribot",
+            description: "Ayvalık'tan 1.5 saat."
+        }],
+        accommodation: [{
+            name: "The Olives Press",
+            type: "Otel",
+            description: "Molivos sahilinde eski bir yağ fabrikasından dönüştürülmüş."
+
+        }],
+        foodAndDrink: [{
+            name: "Vafiros",
+            type: "Taverna",
+            description: "Eşsiz manzarada geleneksel Yunan mutfağı."
+
+        }],
+        activities: [{
+            name: "Uzo Müzesi",
+            description: "Plomari'de uzo üretim sürecini öğrenme."
+
+        }],
+    },
+    {
+        id: "meis-005",
+        name: "Meis (Kastellorizo)",
+        country: "Yunanistan",
+        description: "Kaş'ın hemen karşısında, rengarenk evlerin çevrelediği butik ve huzurlu bir liman adası.",
+        transportation: [{
+            type: "Tekne",
+            description: "Kaş'tan sadece 20 dakika."
+        }],
+        accommodation: [{
+            name: "Mediterraneeo",
+            type: "Butik Otel",
+            description: "Deniz kıyısında karakteristik bir konak."
+
+        }],
+        foodAndDrink: [{
+            name: "Lazarakkis",
+            type: "Restoran",
+            description: "Limanın en ünlü deniz ürünleri restoranı."
+
+        }],
+        activities: [{
+            name: "Mavi Mağara",
+            description: "Botla gidilen dünyanın en ünlü deniz mağaralarından biri."
+
+        }],
+    },
+    {
+        id: "samos-006",
+        name: "Samos (Sisam)",
+        country: "Yunanistan",
+        description: "Pisagor'un evi; yemyeşil ormanları, meşhur şarabı ve harika köyleriyle doğa severlerin favorisi.",
+        transportation: [{
+            type: "Feribot",
+            description: "Kuşadası veya Seferihisar'dan 1-2 saat."
+        }],
+        accommodation: [{
+            name: "Dorysa Seaside",
+            type: "Resort",
+            description: "Pythagoreio yakınında modern ve şık tesis."
+
+        }],
+        foodAndDrink: [{
+            name: "Pera Pana",
+            type: "Taverna",
+            description: "Samos şarabı eşliğinde akşam yemeği."
+
+        }],
+        activities: [{
+            name: "Manolates Köyü",
+            description: "Sanat galerileriyle dolu bir dağ köyü yürüyüşü."
+
+        }],
+    },
+
+]
 
 export default destinations;
