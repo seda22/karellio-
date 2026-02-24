1dc68b5c9744689bdd4bca5998e949c0946d6d3c
seda22 <sedakacarr@gmail.com>
2026-02-24 04:54:48 +0300

    statik destinasyon veri modeli tanımlandı

---
+++ Frontend/src/data/Destinations.js
@@ -0,0 +1,30 @@
+const destinations = [
+    {
+        id: "",
+        name: "",
+        country: "",
+        description: "",
+        transportation: [{
+            type: "",
+            description: ""
+        }],
+        accommodation: [{
+            name: "",
+            type: "",
+            description: ""
+
+        }],
+        foodAndDrink: [{
+            name: "",
+            type: "",
+            description: ""
+
+        }],
+        activities: [{
+            name: "",
+            description: ""
+
+        }],
+    }]
+
+export default destinations;
