3d88513a4dccb268ba1c0da33d65cebae09aa65c
seda22 <sedakacarr@gmail.com>
2026-02-26 06:45:47 +0300

    Island seçildiğinde gösterilecek tanımları ve özellikleri belirleme
    Layout tan iletilen selectedIsland propundaki veriler IslandDetails componentinde bir kısmı konumlandırıldı.
    Seçilen adaya göre detay bilgileri dinamik olarak görüntülenebiliyor.

--- Frontend/src/components/IslandDetails.jsx
+++ Frontend/src/components/IslandDetails.jsx
@@ -1,4 +1,42 @@
 export default function IslandDetails ({selectedIsland}) {
+    console.log(selectedIsland);
+if (!selectedIsland) return <p>Haydi bir ada keşfedelim!</p>;
 
-    return()
+    const {name, country, description, transportation=[], accommodation =[], foodAndDrink, activities} = selectedIsland;
+    
+    return(
+        <div className="island-details">
+            <h2>{name}</h2>
+            <span>{country}</span>
+            <p>{description}</p>
+
+            <h3>Ulaşım</h3>
+            <ul>
+                {transportation.map((item) => {
+                    return(
+                    <li key={item.type}>
+                        <strong>{item.type}:</strong> 
+                        <span>{item.description}</span>
+                    </li>
+                    )
+                })}
+            </ul>
+
+            <h3>Konaklama</h3>
+            <ul>
+                {accommodation.map((item) => {
+                    return(
+                    <li key={item.name}>
+                        <strong>{item.name}</strong>
+                        <span>{item.type}</span>
+                        <p>{item.description}</p>
+                    </li>
+                )   
+                }
+                )}
+            </ul>
+
+
+        </div>
+    )
 }
