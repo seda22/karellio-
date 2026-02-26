c98560109f0ed92173b11674c579cc3e7148d61d
seda22 <sedakacarr@gmail.com>
2026-02-26 04:52:35 +0300

    Child component, parent’ın state’ini gönderilen fonksiyon aracılığıyla günceller.
    -island-card className’ine sahip div’e tıklanırsa onSelect fonksiyonu çalışır.
    -Bu fonksiyon aslında parent’taki setSelectedIsland fonksiyonudur.
    -Böylece selectedIsland state’i tıklanan adanın verisi ile güncellenir.

--- Frontend/src/components/IslandCards.jsx
+++ Frontend/src/components/IslandCards.jsx
@@ -1,9 +1,9 @@
-export default function IslandCards ({destinations}){
+export default function IslandCards ({destinations, onSelect}){
 
     return(
         <div className="card-grid">
             {destinations.map(({id, name, country, description}) =>
-                <div key={id} className="island-card">
+                <div key={id} className="island-card" onClick={ () => {onSelect({id, name, country, description})}}>
                     <h3>{name}</h3>
                     <span>{country}</span>
                     <p>{description}</p>
