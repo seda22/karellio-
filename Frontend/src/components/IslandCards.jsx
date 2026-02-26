d6ce253eb3d0c320d7fda24e3ee792291dfc581b
seda22 <sedakacarr@gmail.com>
2026-02-26 05:38:20 +0300

    Dinamik className ekleme yapıldı.
    -Ternary if ile dinamik classname ekleme ve çıkarma yapıldı.

--- Frontend/src/components/IslandCards.jsx
+++ Frontend/src/components/IslandCards.jsx
@@ -5,7 +5,7 @@ export default function IslandCards ({destinations, onSelect, selectedIsland}){
             {destinations.map(({id, name, country, description}) => {
                 const isSelected = selectedIsland?.id === id;
                 return(
-                <div key={id} className="island-card" onClick={ () => {onSelect({id, name, country, description})}}>
+                <div key={id} className={`island-card ${isSelected ? "selected" : ""}`} onClick={ () => {onSelect({id, name, country, description})}}>
                     <h3>{name}</h3>
                     <span>{country}</span>
                     <p>{description}</p>
