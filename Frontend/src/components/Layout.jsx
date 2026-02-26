0664a4028c5da0539e8aef2ba614dea6ecb10c5a
seda22 <sedakacarr@gmail.com>
2026-02-26 04:43:35 +0300

    State’i parent’a taşıdım (lifting state up) ve child aracılığıyla güncelliyorum.
    -selectedIsland adında bir state tanımladım.Başlangıç değeri null.
    -setSelectedIsland fonksiyonu ile selectedIsland değeri değiştirilebilecek.
    
    -onSelect={setSelectedIsland} fonksiyonunu prop olarak IslandCards componentine gönderiyorum.
    (Parent child’a bir fonksiyon gönderdi.
    Child bu fonksiyonu çağırarak parent’ın state’ini güncelleyebiliyor. Child → Parent communication kurmuş oldum.)

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -1,14 +1,17 @@
 import Header from "./Header";
 import destinations from "../data/Destinations.js"
 import IslandCards from "./IslandCards.jsx"
+import { useState } from "react";
 
 const Layout = () => {
+
+  const [selectedIsland, setSelectedIsland] = useState(null);
 
   return(
     <div className="layout-div">
     <Header/>
     <main className="main">
-        <section className="destination-section"><IslandCards destinations={destinations}/></section>
+        <section className="destination-section"><IslandCards destinations={destinations} onSelect={setSelectedIsland}/></section>
         <section className="content-section">İçerik Alanı</section>
     </main>
     </div>
