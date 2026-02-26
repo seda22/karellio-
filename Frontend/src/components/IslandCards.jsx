026a590190b06e4a0f4de441674384feee224549
seda22 <sedakacarr@gmail.com>
2026-02-26 08:06:44 +0300

    IslandCards bileşeni MUI Joy Card yapısına göre yeniden düzenlendi, CardContent ve Typography kullanıldı

--- Frontend/src/components/IslandCards.jsx
+++ Frontend/src/components/IslandCards.jsx
@@ -2,18 +2,30 @@ import Card from '@mui/joy/Card';
 import CardContent from '@mui/joy/CardContent';
 import Typography from '@mui/joy/Typography';
 
-export default function IslandCards ({destinations, onSelect, selectedIsland}){
+export default function IslandCards({ destinations, onSelect, selectedIsland }) {
 
-    return(
+    return (
         <div className="card-grid">
             {destinations.map((destination) => {
                 const isSelected = selectedIsland?.id === destination.id;
-                return(
-                <div key={destination.id} className={`island-card ${isSelected ? "selected" : ""}`} onClick={ () => {onSelect(destination)}}>
-                    <h3>{destination.name}</h3>
-                    <span>{destination.country}</span>
-                    <p>{destination.description}</p>
-                </div>)})}
+                return (
+                    <Card
+                    key={destination.id}
+                    color={isSelected ? "primary" : "neutral"}
+                    invertedColors={false}
+                    orientation="vertical"
+                    size="lg"
+                    variant={isSelected ? "solid" : "outlined"}
+                    onClick={() => onSelect(destination)} sx={{ cursor: "pointer", transition: "0.3s", "&:hover": { boxShadow: "lg"}}}>
+                    
+                    <CardContent>
+                        <Typography level="h4">{destination.name}</Typography >
+                        <Typography level="body-sm">{destination.country}</Typography>
+                        <Typography level="body-md">{destination.description}</Typography>
+                    </CardContent>
+                    </Card>
+                )
+            })}
         </div>
     )
 }
